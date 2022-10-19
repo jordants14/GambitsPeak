98275a3fa9cfd172765b9a6490645fd0a0d0c693
jordi <jordi@DESKTOP-EJJ9P0F>
2022-10-19 09:16:05 -0400

    Add project files.

---
+++ GambitsPeak/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GambitsPeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+
+        static void Start()
+        {
+            Console.WriteLine("Welcome to Gambit's Peak");
+            Console.WriteLine("Please enter your name:");
+            Console.ReadLine();
+        }
+    }
+}
