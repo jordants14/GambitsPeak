c00ffa2dc14646bd76f727f51fe0a027613053db
Jordan <jordantylersmith14@gmail.com>
2022-10-19 23:35:10 -0400

    Last commit for tonight, updated dialogue.

--- GambitsPeak/Program.cs
+++ GambitsPeak/Program.cs
@@ -50,24 +50,38 @@ namespace GambitsPeak
             Console.Clear();
 
             Console.WriteLine(quote + "Get dressed, then meet me in the living room when you are ready to begin." + quote);
-            Console.WriteLine("Your father hands you a cloak and a rusty dagger.");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to get dressed and go to the living room.");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("You enter the living room and sit on the couch. Your father hands you a map and a rusty dagger.");
             Console.WriteLine("Your father says, " + quote + "it's recommended that you scavenge for items along your journey." + quote);
-            Console.WriteLine(quote + "You may find potions while exploring. If you drink them, they will restore your wellbeing if you get hurt." + quote);
+            Console.WriteLine(quote + "You may find shops while exploring. Visit them and pay coins to obtain healing potions." + quote);
             Console.ReadKey();
             Console.Clear();
 
             Console.WriteLine("Your father then explains that along your way, you will encounter obstacles in your path.");
-            Console.WriteLine("The pathway to Gambit's Peak is filled with citizens that have been cast out of the village.");
-            Console.WriteLine("Many of these citizens are criminals and may cause trouble when trying to pass.");
-            Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("Press any key to leave the village and begin the jounrey to Gambit's Peak.");
+            Console.WriteLine(quote + "The pathway to Gambit's Peak is filled with citizens that have been cast out of the village." + quote);
+            Console.WriteLine("you learn that many of these citizens are criminals and may cause trouble when trying to pass.");
             Console.ReadKey();
             Console.Clear();
 
-            Console.WriteLine("As you leave the village, you stumble upon a small campsite. You go to the campsite to");
-            Console.WriteLine("check for extra supplies to aid you in your jourmey, however you are quickly met with a tall");
-            Console.WriteLine("man who is not happy that you entered his campsite.");
+            Console.WriteLine("Press" + " " + quote + "any key" + quote + " " + "to leave the village and begin the jounrey to Gambit's Peak.");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("You approach the village gate, a guard opens the gate and lets you out. The guard closes the gate behind you.");
+            Console.WriteLine("You look at your map and begin walking on a narrow dirt path. The further you walk away from the village,  ");
+            Console.WriteLine("you begin to see a torch from what looks to be a small campsite in the distance ahead.");
+            Console.WriteLine("You continue on the path until you reach the small campsite. It looks to be abandoned, however the firepit");
+            Console.WriteLine("seems to have been used recently. You start to look around the campsite for supplies to aid you in your journey.");
+            Console.ReadKey();
+            Console.Clear();
+            
+            Console.WriteLine("As you are searching the campsite, you begin to hear footsteps approach.");
+            Console.WriteLine(quote + "WHO ARE YOU!?" + quote + " You turn around to see a very tall man weilding an axe.");
+            Console.WriteLine("You realize quickly that the tall man is hostile and you must fight or run.");
             Console.ReadKey();
             Console.Clear();
         }
